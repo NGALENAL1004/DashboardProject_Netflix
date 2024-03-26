5af430225ce1a5bcd525d9d96d38813f5685064c
ngalenal1004 <ngalenal1004@gmail.com>
2024-03-26 03:44:14 +0000

    second commit

--- dash.py
+++ dash.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="NetflixDashboard", page_icon=":bar_chart:", layout="wide")
 
 # Netflix colors
 primary_red = '#ff0000'
